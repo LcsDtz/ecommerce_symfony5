26bfb69bfefff5da75612997f9a09baaa47786a5
Lucas Dentz <lucas@MacBook-Pro-de-Lucas.local>
2021-01-04 14:43:26 +0100

    add event payement réussi

--- src/Controller/Purchase/PurchasePaymentSuccessController.php
+++ src/Controller/Purchase/PurchasePaymentSuccessController.php
@@ -4,10 +4,12 @@ namespace App\Controller\Purchase;
 
 use App\Entity\Purchase;
 use App\Cart\CartService;
+use App\Event\PurchaseSuccessEvent;
 use App\Repository\PurchaseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PurchasePaymentSuccessController extends AbstractController
@@ -16,7 +18,7 @@ class PurchasePaymentSuccessController extends AbstractController
      * @Route("/purchase/success/{id}", name="purchase_payment_success")
      * @IsGranted("ROLE_USER")
      */
-    public function success($id, PurchaseRepository $purchaseRepository, EntityManagerInterface $em, CartService $cartService)
+    public function success($id, PurchaseRepository $purchaseRepository, EntityManagerInterface $em, CartService $cartService, EventDispatcherInterface $dispatcher)
     {
         $purchase = $purchaseRepository->find($id);
 
@@ -33,6 +35,9 @@ class PurchasePaymentSuccessController extends AbstractController
         $em->flush();
 
         $cartService->empty();
+
+        $purchaseEvent = new PurchaseSuccessEvent($purchase);
+        $dispatcher->dispatch($purchaseEvent, 'purchase.success');
 
         $this->addFlash('success', "La commande a bien été payée et confirmée");
         return $this->redirectToRoute('purchase_index');
